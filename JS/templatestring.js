8a5f45d92509c061bccce2c9dfcb08434d09e786
iprankurpandey <pprankur@gmail.com>
2022-02-26 19:26:39 +0530

    done with ts

---
+++ JS/templatestring.js
@@ -0,0 +1,12 @@
+console.log(`this is example of template string`)
+
+let add = 56;
+console.log(`the value of add is ${add}`)
+
+const add1=(a,b)=>{
+     c = a+b
+     console.log(`the value of c is ${c}`)
+     return c
+}
+
+add1(10,20)
