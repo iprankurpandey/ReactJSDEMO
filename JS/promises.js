624cda85289ac1dfe2b8f7dfe3a7c785406823f3
iprankurpandey <pprankur@gmail.com>
2022-02-28 16:00:40 +0530

    done

--- JS/promises.js
+++ JS/promises.js
@@ -1,3 +1,18 @@
 console.log(`promises`)
 
-let promise = new Promise()
+function promise(value) {
+    console.log('fetching data please wait')
+    return new Promise(function (resolve, reject) {
+
+        setTimeout(() => {
+            if (value) {
+                resolve("I am successful")
+            } else {
+                reject('I am failed')
+            }
+        }, 3000);
+    })
+}
+let onFulfillment = (result) => console.log(result)
+let onRejection = (error) => console.log(error)
+promise(false).then(onFulfillment).catch(onRejection)
